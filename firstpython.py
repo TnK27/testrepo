fd07ed4ac5df7c3736263483cfc5a721125ede5d
TnK27 <cartertc.90@gmail.com>
2025-02-13 20:31:29 -0600

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,10 @@
+#Display the output
+juice = ["orange", "apple", "green", "cranberry", "grape"]
+names = ["Taylor", "John", "Jaylen", "Nico," "Damon"]
+
+for name in names:
+  introduction = f/"My name is {title.name}!"/n 
+    for fruit in juice:
+      favorite_fruit = "My favorite juice is {title.fruit} Juice!"
+      print(introduction)
+      print(favorite_fruit)
